3013fd1a3cff794a447d52a5c7ba0b58643d6c9d
Mohit sharma <mohitkumarsharma40@gmail.com>
2022-09-29 14:46:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import ProductCard from './Components/Product-Page-Component/ProductCard';
-
 function App() {
   return (
     <div className="App">

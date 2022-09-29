dab7f2f78e130db68033fadccb52bd0409798ea4
Mohit sharma <mohitkumarsharma40@gmail.com>
2022-09-29 14:55:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Navbar from './Components/Navbar';
 import SinglePage from './Components/SinglePage';
 import ProductCard from './Components/Product-Page-Component/ProductCard';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <SinglePage/>
       <ProductCard/>
     </div>

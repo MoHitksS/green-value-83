75502464f69454caf10a16e738163ecd2cef9188
unknown <ravindra111223@gmail.com>
2022-09-29 23:23:02 +0530

    addingday-3 tasks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import AllRoutes from './Routes/AllRoutes';
 import "swiper/css/bundle";
+import Cart from './Components/Pages/Cart';
+import Checkout from './Components/Pages/Checkout';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <AllRoutes/>
+    <Cart />
+      {/* <Navbar/> */}
+      {/* <AllRoutes/> */}
     </div>
   );
 }

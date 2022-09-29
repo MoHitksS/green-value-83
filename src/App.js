19094ee292b3007286bac8bd0b3c08bbc64a560c
Mohit sharma <mohitkumarsharma40@gmail.com>
2022-09-29 14:53:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import './App.css';
+import Navbar from './Components/Navbar';
+import SinglePage from './Components/SinglePage';
+import ProductCard from './Components/Product-Page-Component/ProductCard';
 
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <SinglePage/>
+      <ProductCard/>
+    </div>
+  );
+}
 
-export default App;
+export default App;=

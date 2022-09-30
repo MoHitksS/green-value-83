45152583b2926582e28942b72d04e53500df780e
Mohit sharma <mohitkumarsharma40@gmail.com>
2022-09-30 11:40:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import AllRoutes from './Routes/AllRoutes';
 import "swiper/css/bundle";
-import Cart from './Components/Pages/Cart';
-import Checkout from './Components/Pages/Checkout';
 function App() {
   return (
     <div className="App">
-    <Cart />
-      {/* <Navbar/> */}
-      {/* <AllRoutes/> */}
+      <Navbar/> 
+      <AllRoutes/> 
     </div>
   );
 }

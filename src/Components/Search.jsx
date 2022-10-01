7bd157271d0eae07cf25b88e10afec4da6c0844c
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-10-01 12:51:58 +0530

    chamges made

---
+++ src/Components/Search.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components'
+import { getProduct } from '../Redux/App/action';
+import ProductPage from '../Routes/ProductPage';
+const Search = () => {
+    const [text, setText] = useState('');
+    const dispatch = useDispatch();
+    const { products } = useSelector((store) => (store.AppReducer))
+    const handleSearch = (e) => {
+        if (e.key === 'Enter') {
+            if (text) {
+                dispatch(getProduct(`products?q=${text}`))
+            }
+        }
+    }
+    console.log(products)
+    return (
+        <>
+            <Container>
+                <div className='searchBox'>
+                    <input type="text" placeholder='Enter Search Term' value={text} onChange={(e) => setText(e.target.value)} onKeyUp={(e) => handleSearch(e)} />
+                    {products.length > 0 && <p style={{ textAlign: "left" }}>{products.length} Result Shown</p>}
+                </div>
+
+                <div className='productSection'>
+                    <ProductPage />
+                </div>
+            </Container>
+
+        </>
+    )
+}
+
+const Container = styled.div`
+    width:100%;
+    margin-top:150px;
+    position:absolute;
+    background-color:white;
+    
+
+    .searchBox{
+        width:90%;
+        margin:auto;
+        
+        left:5%;
+        position:fixed;
+        padding-top:20px;
+        background-color:white;
+        z-index:4;
+    }
+
+    .searchBox>input{
+        width:100%;
+        height:30px;
+        margin:auto;
+        outline:none;
+        border:none;
+        border-bottom:1px solid grey;
+        font-size:17px;
+        background-color:white;
+    }
+
+    .searchBox>input::placeholder{
+        text-transform:uppercase;
+        font-size:17px;
+    }
+
+    .productSection{
+        margin-top:80px;
+        margin-bottom:100px;
+    }
+
+`
+export default Search
